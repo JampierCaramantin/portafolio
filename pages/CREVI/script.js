382f1ae597eaa65160a14bbb0712706393690e51
Jampier Caramantin <jampiercaramantin06@gmail.com>
2025-03-21 21:17:38 -0500

    Eliminando referencias a submódulo

---
+++ pages/CREVI/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtén todos los enlaces de navegación
+    const links = document.querySelectorAll('nav a');
+
+    // Escucha el evento click en cada enlace
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault(); // Evita el comportamiento predeterminado del enlace
+            const targetId = link.getAttribute('href').substring(1); // Obtén el id del objetivo sin el "#" al principio
+            const targetElement = document.getElementById(targetId); // Encuentra el elemento de destino
+
+            // Desplázate suavemente al elemento de destino
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+// para cuando estoy en una paguina diferente a index y quiero ir a contacto
+
